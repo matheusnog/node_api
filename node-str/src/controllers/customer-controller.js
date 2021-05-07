c5f9fd9b6fc0fb4c5bf30d16b84abb771956c75c
matheusnog <matheusnogueira450@gmail.com>
2021-05-07 10:32:36 -0300

    curso finalizado

---
+++ node-str/src/controllers/customer-controller.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/customer-repository');
+const md5 = require('md5');
+const authService = require('../services/auth-service');
+
+const emailService = require('../services/email-service');
+
+exports.post = async (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.name, 3, 'O nome deve conter pelo menos 3 caracteres');
+    contract.isEmail(req.body.email, 'E-mail inválido');
+    contract.hasMinLen(req.body.password, 6, 'O senha deve conter pelo menos 6 caracteres');
+
+    // Se os dados forem inválidos
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
+    try {
+        await repository.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ["user"]
+        })
+
+        // envio de email
+        // emailService.send(req.body.email, 'Bem vindo ao Node Store', global.EMAIL_TMPL.replace('{0}', req.body.name))
+
+        res.status(201).send({ message: 'cliente cadastrado com sucesso' });
+    } catch (e) {
+        res.status(500).send({
+            message: 'falha ao processar a requisição'
+        });
+    }
+
+};
+
+exports.authenticate = async (req, res, next) => {
+    try {
+        const customer = await repository.authenticate({            
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        })
+
+        if(!customer){
+            res.status(404).send({message: 'usuario ou senha invalidos'});
+            return;
+        }
+
+        const token = await authService.generateToken({id: customer._id, email: customer.email, name: customer.name, roles: customer.roles});
+
+        // envio de email
+        // emailService.send(req.body.email, 'Bem vindo ao Node Store', global.EMAIL_TMPL.replace('{0}', req.body.name))
+
+        res.status(201).send({
+            token: token, 
+            data: {
+                email: customer.email, 
+                name: customer.name
+            }
+         });
+    } catch (e) {
+        res.status(500).send({
+            message: 'falha ao processar a requisição'
+        });
+    }
+
+};
+
+exports.refreshToken = async (req, res, next) => {
+    try {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        const data = await authService.decodeToken(token);
+
+        const customer = await repository.getById(data.id);
+
+        if(!customer){
+            res.status(404).send({message: 'cliente não encontrado'});
+            return;
+        }
+
+        const tokenData = await authService.generateToken({id: customer._id, email: customer.email, name: customer.name, roles: customer.roles});
+
+        // envio de email
+        // emailService.send(req.body.email, 'Bem vindo ao Node Store', global.EMAIL_TMPL.replace('{0}', req.body.name))
+
+        res.status(201).send({
+            token: token, 
+            data: {
+                email: customer.email, 
+                name: customer.name
+            }
+         });
+    } catch (e) {
+        res.status(500).send({
+            message: 'falha ao processar a requisição'
+        });
+    }
+
+};
